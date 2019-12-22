16b9c0dbb8bcbb5f373148d9a24f5cb736bab3b4
Saenko Evgeniy <e.saenko@rambler-co.ru>
2019-12-22 18:41:41 +0300

    Minor map edits

--- src/game/animations/animations.player.ts
+++ src/game/animations/animations.player.ts
@@ -2,23 +2,19 @@ import { Animation, AnimationGroup } from 'core/assets/animation';
 
 export const playerAnimation = new AnimationGroup({
   top: new Animation({
-    frames: [6,7,8],
-    speed: 1,
-    defaultFrame: 6,
-  }),
-  down: new Animation({
-    frames: [0,1,2],
-    speed: 1,
-    defaultFrame: 0,
+    frames: [[0,8], [1,8], [2,8], [3,8], [4,8], [5,8], [6,8], [7,8], [8,8]],
+    speed: 2,
   }),
   left: new Animation({
-    frames: [9,10,11],
-    speed: 1,
-    defaultFrame: 9,
+    frames: [[0,9], [1,9], [2,9], [3,9], [4,9], [5,9], [6,9], [7,9], [8,9]],
+    speed: 2,
   }),
   right: new Animation({
-    frames: [3,4,5],
-    speed: 1,
-    defaultFrame: 3,
+    frames: [[0,11], [1,11], [2,11], [3,11], [4,11], [5,11], [6,11], [7,11], [8,11]],
+    speed: 2,
   }),
+  down: new Animation({
+    frames: [[0,10], [1,10], [2,10], [3,10], [4,10], [5,10], [6,10], [7,10], [8,10]],
+    speed: 2,
+  })
 });
--- src/game/scenes/GameScene.ts
+++ src/game/scenes/GameScene.ts
@@ -2,12 +2,14 @@ import { Scene } from 'core/scene';
 import { AnimationGroup, Animation } from 'core/assets/animation';
 import { Camera } from 'core/camera';
 
+import * as tilemap from 'game/levels/defaultScene.json';
+import { playerAnimation } from 'game/animations/animations.player';
+
 import {Ground, Player, Fps} from 'game/entities';
 // @ts-ignore
 import footmanSprite from 'game/assets/footman.png';
 // @ts-ignore
 import atlas from 'game/assets/summer_atlas.png';
-import * as tilemap from 'game/levels/defaultScene.json';
 
 export const GameScene = new Scene({
   fill: '#000',
@@ -43,24 +45,6 @@ export const GameScene = new Scene({
         id: 'fps_meter',
         content: 0,
       }]
-    });
-    const playerAnimation = new AnimationGroup({
-      top: new Animation({
-        frames: [[0,8], [1,8], [2,8], [3,8], [4,8], [5,8], [6,8], [7,8], [8,8]],
-        speed: 2,
-      }),
-      left: new Animation({
-        frames: [[0,9], [1,9], [2,9], [3,9], [4,9], [5,9], [6,9], [7,9], [8,9]],
-        speed: 2,
-      }),
-      right: new Animation({
-        frames: [[0,11], [1,11], [2,11], [3,11], [4,11], [5,11], [6,11], [7,11], [8,11]],
-        speed: 2,
-      }),
-      down: new Animation({
-        frames: [[0,10], [1,10], [2,10], [3,10], [4,10], [5,10], [6,10], [7,10], [8,10]],
-        speed: 2,
-      })
     });
     const playerSprite = scene.assets.getSprite('footman');
     playerSprite.useAnimation(playerAnimation);
